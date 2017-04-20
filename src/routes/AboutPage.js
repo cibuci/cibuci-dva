13ed6053017c48edf19a9cdde1342d5fc48ea1ad
Wayne <evilcome.com@gmail.com>
2017-04-20 22:58:24 +0800

    add COOL logo animation

--- src/routes/AboutPage.js
+++ src/routes/AboutPage.js
@@ -4,10 +4,14 @@ import styles from './IndexPage.less';
 import Layout from '../components/Layout';
 import { Progress, Level, LevelItem, Heading, Title, Group, Input, Button, Message } from 're-bulma';
 import Comment from '../components/Comment';
+import LogoGather from '../components/LogoGather';
 
 function AboutPage() {
   return (
     <Layout noheader>
+      <div style={{ width: '100%', height: '100%'}}>
+        <LogoGather pixSize={20} pointSizeMin={10} />
+      </div>
     </Layout>
   );
 }
