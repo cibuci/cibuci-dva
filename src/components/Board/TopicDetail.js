af3ff6be87b8b130252c203e2312c8a6ca8e196d
Wayne <evilcome.com@gmail.com>
2017-04-24 19:14:40 +0800

    add back button

--- src/components/Board/TopicDetail.js
+++ src/components/Board/TopicDetail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from 'antd';
 import { connect } from 'dva';
 
 const styles = {
@@ -11,16 +12,25 @@ const styles = {
   },
 };
 
-const TopicDetail = ({ topic }) => {
+const TopicDetail = (props, context) => {
+  const { topic } = props;
+
   if (!topic) return null;
   const { title } = topic;
 
   return (
-    <p style={styles.title}>{title}</p>
+    <p style={styles.title}>
+      <Button icon="left" onClick={context.router.goBack} />
+      {title}
+    </p>
   );
 };
 
 TopicDetail.propTypes = {
+};
+
+TopicDetail.contextTypes = {
+  router: React.PropTypes.object,
 };
 
 function mapStateToProps(state) {
