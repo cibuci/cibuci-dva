5e27763bf0b7aacb33486218c1882e7bc8e63ee8
Wayne <evilcome.com@gmail.com>
2017-05-14 19:06:49 +0800

    modify the theme color

--- theme.js
+++ theme.js
@@ -1,8 +1,7 @@
 module.exports = () => {
   return {
-    '@primary-color': '#AA6708',
-    '@link-color': '#AA6708',
-    '@btn-primary-bg': '#AA6708',
+    '@primary-color': '#222',
+    '@link-color': '#222',
     '@font-family': '"Source Sans Pro, Helvetica Neue, Arial, sans-serif"',
     '@border-radius-base': '2px',
   };
